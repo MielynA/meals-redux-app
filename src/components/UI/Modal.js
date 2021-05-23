a482a95a4a1d96184fdaaa6c3c5e993b5ca1041a
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2021-05-22 22:23:07 -0500

    fix the classes on the backdrop

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,12 +3,12 @@ import { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 const BackDrop = (props) => {
-  return <div className={classes.BackDrop}></div>;
+  return <div className={classes.backdrop}></div>;
 };
 
 const ModalOverlay = (props) => {
   return (
-    <div className={classes.ModalOverlay}>
+    <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
     </div>
   );
@@ -20,7 +20,6 @@ const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(<BackDrop />, portalElement)}
-
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
