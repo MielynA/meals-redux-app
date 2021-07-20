fd3587db9383daa367292b3b9f5fe818a9d7e711
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2021-07-20 00:01:50 -0400

    added useEffect and use state in bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHiglighted, setBtnIsHiglighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((cur, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((cur, item) => {
     return cur + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHiglighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHiglighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />{" "}
       </span>
